31653b950c720f8b0eeca8f3ea50729958ea46dd
Lenz Fiedler <l.fiedler@hzdr.de>
2023-03-10 14:14:19 +0100

    Finished with data generation.

--- data_generation/dft_largeK.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-/home/fiedlerl/tools/qe_development_branch/PW/src/pw.x -in Be_snapshot0_largeK.pw.scf.in
-/home/fiedlerl/tools/qe_development_branch/PP/src/pp.x -in Be_snapshot0.pp.ldos.in
