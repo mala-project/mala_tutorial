06148b70cbfa5bce90c573ae6c8997bd630206bb
Lenz Fiedler <l.fiedler@hzdr.de>
2023-03-20 11:25:20 +0100

    Added cleanup file

---
+++ cleanup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+rm *.npy
+rm *.zip
+rm *.cube
+rm *.tmp
+rm *.in
+rm *.json
+rm -r data_generation/temp
+rm data_generation/tmp.pp*
+rm data_generation/*.out
+rm data_generation/*.npy
